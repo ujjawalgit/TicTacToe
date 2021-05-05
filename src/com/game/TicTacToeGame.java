88e74de06325276a07577a92252a9b536bbf8780
ujjawalgit <ujjawalkumarsingh08@gmail.com>
2021-05-05 17:37:40 +0530

    added: Create a method to allow player to input X or O

--- src/com/game/TicTacToeGame.java
+++ src/com/game/TicTacToeGame.java
@@ -1,7 +1,19 @@
 package com.game;
 
 public class TicTacToeGame {
+
+    /*creating board with empty spaces to play  game*/
+    private static char[] createBoard() {
+        char[] board = new char[10];
+        for (int i = 1; i < 10; i++) {
+            board[i] = ' ';
+        }
+        return board;
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to  TicTacToeGame");
+        /*createBoard method will create board with empty spaces*/
+        char[] board = createBoard();
     }
 }
